4a0d84bf8f1cb6e44cc8cc02a01a3a6870aa8b93
Jakub Janšta <kubulambula@gmail.com>
2025-01-08 19:07:14 +0100

    Add files via upload

--- charlesdarwin1st/scenes/player.gd
+++ charlesdarwin1st/scenes/player.gd
@@ -4,19 +4,31 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+var gravity = Vector2(0, 350)
+
+var hp = 100
+
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	UI.ui_scene.set_hp(hp)
+	 Add the gravity.
+	#if not is_on_floor():
+	velocity += gravity * delta
+	
+	if Input.is_action_just_pressed("take_damage"):
+		hp = hp - 10
+	
+	if Input.is_action_just_pressed("flip_gravity"):
+		gravity = -gravity
+		$AnimatedSprite2D.flip_v = not $AnimatedSprite2D.flip_v
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	# Handle jump
+	if Input.is_action_just_pressed("ui_up") and (is_on_floor() or is_on_ceiling()):
+		velocity=JUMP_VELOCITY * gravity.normalized()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		$AnimatedSprite2D.play("run")
 		velocity.x = direction * SPEED
