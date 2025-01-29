a67923b0b0f7ca92d5387978fd8baa7f86e11878
Jakub Janšta <kubulambula@gmail.com>
2025-01-29 18:50:40 +0100

    Add files via upload

--- charlesdarwin1st/scenes/player.gd
+++ charlesdarwin1st/scenes/player.gd
@@ -4,6 +4,13 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -200.0
 
+var step_sounds = [
+	load("res://assets/step_1.wav"),
+	load("res://assets/step_2.wav"),
+	load("res://assets/step_3.wav"),
+	load("res://assets/step_4.wav"),
+]
+
 var gravity = Vector2(0, 500)
 
 var hp = 100
@@ -34,10 +41,21 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
+		if $Timer.is_stopped():
+			$Timer.wait_time = 0.2
+			$Timer.start()
+			play_random_sound(step_sounds)
 		$AnimatedSprite2D.play("run")
 		velocity.x = direction * SPEED
 	else:
 		$AnimatedSprite2D.play("idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
 	move_and_slide()
+
+
+func play_random_sound(array):
+	randomize()
+	var random_number = randi_range(0, array.size() - 1)
+	$AudioStreamPlayer2D.stream = array[random_number]
+	$AudioStreamPlayer2D.play()
