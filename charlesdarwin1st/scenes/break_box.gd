87cf75e3bc24f4e9efe2c5fdd27fbaa25b6bef01
Jakub Janšta <kubulambula@gmail.com>
2025-04-02 19:05:16 +0200

    Add files via upload

--- charlesdarwin1st/scenes/break_box.gd
+++ charlesdarwin1st/scenes/break_box.gd
@@ -1,11 +1,14 @@
 extends StaticBody2D
 
+func _process(delta: float) -> void:
+	pass
+
 
 func _on_area_2d_body_entered(body):
 	if body.current_character == "karel":
+		$AudioStreamPlayer2D.play()
 		process_mode = ProcessMode.PROCESS_MODE_DISABLED
 		visible = false
-		$AudioStreamPlayer2D.play()
 	else:
 		$AnimationPlayer.play("shake")
 
@@ -13,3 +16,7 @@ func _on_area_2d_body_entered(body):
 func _on_audio_stream_player_2d_finished() -> void:
 	print("sound finished")
 	queue_free()
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	$AnimationPlayer.stop()
